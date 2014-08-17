22d8730da9c7125fc291a3a85a55dc3d7404cce9
Derek Mulcahy <derekmulcahy@gmail.com>
2014-08-17 16:18:07 -0400

    [KINETIS] MCHCK board added.

---
+++ demos/KINETIS/MCHCK-K20/main.c
@@ -0,0 +1,84 @@
+/*
+    ChibiOS/RT - Copyright (C) 2006-2014 Giovanni Di Sirio
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include "ch.h"
+#include "hal.h"
+#include "test.h"
+
+__attribute__ ((section(".cfmconfig")))
+const uint8_t _cfm[0x10] = {
+  0xFF,  /* NV_BACKKEY3: KEY=0xFF */
+  0xFF,  /* NV_BACKKEY2: KEY=0xFF */
+  0xFF,  /* NV_BACKKEY1: KEY=0xFF */
+  0xFF,  /* NV_BACKKEY0: KEY=0xFF */
+  0xFF,  /* NV_BACKKEY7: KEY=0xFF */
+  0xFF,  /* NV_BACKKEY6: KEY=0xFF */
+  0xFF,  /* NV_BACKKEY5: KEY=0xFF */
+  0xFF,  /* NV_BACKKEY4: KEY=0xFF */
+  0xFF,  /* NV_FPROT3: PROT=0xFF */
+  0xFF,  /* NV_FPROT2: PROT=0xFF */
+  0xFF,  /* NV_FPROT1: PROT=0xFF */
+  0xFF,  /* NV_FPROT0: PROT=0xFF */
+  0xBE,  /* NV_FSEC: KEYEN=1,MEEN=3,FSLACC=3,SEC=2 */
+  0xFF,  /* NV_FOPT: ??=1,??=1,FAST_INIT=1,LPBOOT1=1,RESET_PIN_CFG=1,
+                      NMI_DIS=1,EZPORT_DIS=1,LPBOOT0=1 */
+  0xFF,
+  0xFF
+};
+
+static THD_WORKING_AREA(waThread1, 64);
+static THD_FUNCTION(Thread1, arg) {
+
+  (void)arg;
+  chRegSetThreadName("Blinker");
+  while (TRUE) {
+    palTogglePad(GPIOB, GPIOB_LED);
+    chThdSleepMilliseconds(300);
+  }
+
+  return 0;
+}
+
+/*
+ * Application entry point.
+ */
+int main(void) {
+
+  /*
+   * System initializations.
+   * - HAL initialization, this also initializes the configured device drivers
+   *   and performs the board-specific initializations.
+   * - Kernel initialization, the main() function becomes a thread and the
+   *   RTOS is active.
+   */
+  halInit();
+  chSysInit();
+
+  /*
+   * Activates serial 1 (UART0) using the driver default configuration.
+   */
+  sdStart(&SD1, NULL);
+
+  /*
+   * Creates the blinker threads.
+   */
+  chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
+
+  TestThread(&SD1);
+  while (1) {
+    chThdSleepMilliseconds(500);
+  }
+}
