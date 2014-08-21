b6aaef174edbf32b995d1b8f78da09c257cf2d63
Derek Mulcahy <derekmulcahy@gmail.com>
2014-08-21 18:46:45 -0400

    Comment out kl25z.h contents for Eclipse CDT sanity

--- os/ext/CMSIS/KINETIS/kl25z.h
+++ os/ext/CMSIS/KINETIS/kl25z.h
@@ -1,8 +1,9 @@
+#if 0
 /*
- * Copyright © 2013-2014 Fabio Utzig, http://fabioutzig.com
+ * Copyright (C) 2013-2014 Fabio Utzig, http://fabioutzig.com
  *
  * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the “Software”),
+ * a copy of this software and associated documentation files (the "Software"),
  * to deal in the Software without restriction, including without limitation
  * the rights to use, copy, modify, merge, publish, distribute, sublicense,
  * and/or sell copies of the Software, and to permit persons to whom the
@@ -11,7 +12,7 @@
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
  * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
@@ -1144,4 +1145,5 @@ typedef struct
 #define UARTx_C5_BOTHEDGE            ((uint8_t)0x02)    /*!< Both Edge Sampling */
 #define UARTx_C5_RESYNCDIS           ((uint8_t)0x01)    /*!< Resynchronization Disable */
 
+#endif
 #endif
